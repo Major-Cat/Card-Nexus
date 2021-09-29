0f41fa8d9360a6af51b2416636d897a0f3108d06
Major-Cat <ewanjreid@gmail.com>
2021-09-29 15:02:05 +0100

    More of trying to get the button and inputs to work

--- front_page.py
+++ front_page.py
@@ -1,31 +1,33 @@
 from ursina import *
-#from ursina.prefabs.dropdown_menu import DropdownMenu, DropdownMenuButton
 
-temp_users = ["jim","john","james"]
 app = Ursina()
 
 class t_button(Button):
-	def __init__(self,texture = "white_cube",x=5,y=0,New_name=None,New_pass=None):
+	def __init__(self,texture = "white_cube",x=6.5,y=0,New_name=None,New_pass=None):
+		self.New_name,self.New_pass = New_name, New_pass
 		super().__init__(
 			parent = scene,
-			color = color.white,
+			text = "sign up",
+			color = color.gray,
 			texture = texture,
 			highlight_color = color.light_gray,
-			pressed_color = color.gray,
-			position = (x,y)
+			pressed_color = color.dark_gray,
+			position = (x,y),
+			scale = (1.5,1)
 			)
 
 	def input(self,key):
 		if self.hovered:
 			if key == 'left mouse down':
-				return self.New_name.text,self.New_pass.text
+				#print(self.New_name.text,",",self.New_pass.text)
+				return self.New_name.text,",",self.New_pass.text
 
 
 Text.size = 0.02
 Text.default_resolution = 1080 * 0.05
 
-new_name = InputField(y=-.12,x=.5,limit_content_to="1234567890-_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@;:?.,!*% ")
-new_pass = InputField(y=-.18,x=.5,limit_content_to="1234567890-_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@;:?.,!*% ",hide_content=True)
+new_name = InputField(y=.02,x=.45,limit_content_to="1234567890-_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@;:?.,!*% ")
+new_pass = InputField(y=-.04,x=.45,limit_content_to="1234567890-_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@;:?.,!*% ",hide_content=True)
 new_name.next_field = new_pass
 
 back1 = Entity(parent=scene,model="quad",position=(-3.6,1.6,0.1),scale=(3,2))
@@ -35,20 +37,14 @@ back2 = Entity(parent=scene,model="quad",position=(3.6,1.6,0.1),scale=(3,2))
 sign_up = Text(text="sign up",scale = 15,parent = scene, origin=(-3.5,-5), background=False,color = Color(0,0,0,0.8))
 sign_up_button = t_button(New_name = new_name, New_pass = new_pass)
 
-
-#def update():
-	#global New_name
-	#New_name = new_name.text
-	#print(New_name,"New_name")
-	#print(selected_user)
-	#added_users = DropdownMenu('Users', buttons=tuple([DropdownMenuButton(user) for user in temp_users]),parent = scene,position=(-5,0.5),scale = (2.5,0.5))
-	#added_users.input(held_keys)
+def update():
+	if held_keys == "left mouse down":
+		print("hello world")
+		new_dude = sign_up_button.input(held_keys)
+		print(new_dude,"new dude")
 
 
 
-#username_field = InputField(y=-.12, limit_content_to='0123456789')
-#password_field = InputField(y=-.18, hide_content=True)
-#username_field.next_field = password_field
 
 
 app.run()
