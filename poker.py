809de11a3546d8f0bdb3f392a28cb6b4b67bf450
Major-Cat <ewanjreid@gmail.com>
2022-01-15 11:38:14 +0000

    Allowed menu to link to Poker

--- poker.py
+++ poker.py
@@ -1,6 +1,11 @@
 import cards
+from ursina import *
 
 l = cards.Deck() # cards.deck here will be replaced with the object that is the deck in poker.py (this is temporary)
+
+class my_button(Button):
+	def __init__(self,x=3.5,y=0,message="",width = 2):
+		super().__init__(parent = scene, text = message, color = color.light_gray, texture = "white_cube", highlight_color = color.white, pressed_color = color.dark_gray, position = (x,y), scale = (width,0.5))
 
 class Community_Cards():
 	def __init__(self,Cards=[]):
@@ -8,12 +13,11 @@ class Community_Cards():
 
 
 		
-
-
 class Hand():
 	def __init__(self,Cards=[]):
 		self.Cards = Cards
 		self.Cards.append(l.draw())
 		self.Cards.append(l.draw())
 
-print(Hand().Cards)
+def run(app):
+	print(Hand().Cards)
