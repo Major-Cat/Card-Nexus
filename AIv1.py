e5ef28d9645729cadc7aa02f8198ff2167bad61f
Frazzle-Dazzle <fraserreid78@googlemail.com>
2022-02-05 00:10:00 +0000

    Obama's Sigmoid Grindset
    Added recursive thinking to Obama so he can handle all possible card quantities. Added extensive game simulation testing to Ai file for debugging behaviour. Prevented scoring function from selecting the community as the winner, I think...

--- AIv1.py
+++ AIv1.py
@@ -1,11 +1,10 @@
-from cards import *
-from json import load
+from json import load as jload
 from random import choices
 from pickle import load, loads
 from gzip import decompress
 from math import exp
+from itertools import combinations
 
-deck = Deck()
 
 
 def think(hand, community):
@@ -17,6 +16,9 @@ def think(hand, community):
 		return formated_cards
 
 	def sigmoid(x):
+		# After lots of simulated games I believe Obama is naturally a coward.
+		# We should pass money variables to this sigmoid function to change his decisions.
+		# I think this will make Obama a true Sigmoid male. -FSR
 		k = 0.1 # logistic growth rate
 		x0 = 40 # Sigmoids midpoint
 		L = 100 # Curves maximum value
@@ -29,50 +31,127 @@ def think(hand, community):
 			temp_list.append(str(str(temp_value)+str(card.suit)))
 		if len(list_of_cards) == 2:
 			return str(sorted(temp_list)[0]+sorted(temp_list)[1]) # Use this return for Pre-flop
-		elif len(list_of_cards) == 5:
+		else:
 			string = ''
 			for x,i in enumerate(temp_list): 
 				string += str(sorted(temp_list)[x])
-			print(string)
 			return string # Use this return for Post-flop
 
+	def postflop_evaluate(hand):
+		hand_string = sort_hand_into_string(hand)
+		with open('Postflop_lookup.pkl', 'rb') as f: # Opens compressed binary file
+			lookup = load(f) # Loads compressed binary dictionary to memory
+		lookup = decompress(lookup) # Decompresses compressed binary dictionary
+		lookup = loads(lookup) # Returns python dict object from bytes
+		confidence = lookup['Hands'][hand_string]
+		return confidence
+
+	print(f'Hand = {sort_hand_into_string(hand+community)}')
 	if len(community) > 0: # Preflop check
 		if len(community) == 3: # Round 1 check
 			hand = hand + community
-			hand_string = sort_hand_into_string(hand)
-			with open('Postflop_lookup.pkl', 'rb') as f: # Opens compressed binary file
-				lookup = load(f) # Loads compressed binary dictionary to memory
-			lookup = decompress(lookup) # Decompresses compressed binary dictionary
-			lookup = loads(lookup) # Returns python dict object from bytes
-			confidence = lookup['Hands'][hand_string]
-			print(f"Obama's confidence = {confidence}%")
-			print(f"Obama's is {round(sigmoid(confidence),2)}% likely to keep playing.\n")
+			confidence = postflop_evaluate(hand)
+		else:
+			confidence = max(postflop_evaluate(combo) for combo in combinations(hand + community, 5)) # This is very slow (eval takes 1.2s and is run 21 times for 7 card scenarios)
+		print(f"Obama's confidence = {confidence}%")
+		print(f"Obama's is {round(sigmoid(confidence),2)}% likely to keep playing.")
+		options = ['Play', 'Fold']
+		weights = [sigmoid(confidence), 100-sigmoid(confidence)]
+		choice = choices(options, weights)
+		print(f"Obama has chosen to {choice}.\n")
+		return choice
+
 
 	else: # Preflop
-		with open('preflop_lookup.json', 'r') as f:
-			lookup = load(f)
+		with open(r'Storage\preflop_lookup.json', 'r') as f:
+			lookup = jload(f)
 		lookup = dict(sorted(lookup['Hands'].items(), key=lambda item: item[1]))
 		hand_count = len(lookup.items())
 		hand_index = list(lookup.keys()).index(sort_hand_into_string(hand))
-		play_weight = (hand_index/hand_count)*100
-		print(round(play_weight, 1))
+		confidence = (hand_index/hand_count)*100
+		print(f'Pre-flop confidence = {round(confidence, 1)}%')
 		options = ['Play', 'Fold']
-		weights = [play_weight, 100 - play_weight]
-		print(choices(options, weights))
+		weights = [confidence, 100 - confidence]
+		choice = choices(options, weights)
+		print(f'Obama has chosen to {choice}\n')
+		return choice
 
 
-for j in range(10): # For debugging: prints the AI weighting of 20 different hands.
+# TESTING #
+from cards import *
+
+deck = Deck()
+community = []
+hands = []
+player_count = 8
+
+#round 0
+print('\nRound 0')
+
+for i in range(player_count):
 	hand = []
-	community = []
-
-	for i in range(2):
+	for j in range(2):
 		hand.append(deck.draw())
+	hands.append((i, hand))
 
-	for i in range(3):
-		community.append(deck.draw())
+hands[:] = [hand for _,hand in hands if not think(hand, community) == ['Fold']]
 
 
-	think(hand, community)
+print(f'After pre-flop, {len(hands)} players remain')
+
+if len(hands) == 0:
+	print('Everyone has folded, the game is over.')
+	quit()
+elif len(hands) == 1:
+	print('Only one player remains, they have won.')
+	quit()
+
+for i in range(3):
+	community.append(deck.draw())
+
+print('\nRound 1')
+
+
+hands[:] = [hand for hand in hands if not think(hand, community) == ['Fold']]
+
+print(f'After round 1, {len(hands)} players remain')
+
+if len(hands) == 0:
+	print('Everyone has folded, the game is over.')
+	quit()
+elif len(hands) == 1:
+	print('Only one player remains, they have won.')
+	quit()
+
+community.append(deck.draw())
+
+print('\nRound 2')
+
+hands[:] = [hand for hand in hands if not think(hand, community) == ['Fold']]
+
+print(f'After round 2, {len(hands)} players remain')
+
+if len(hands) == 0:
+	print('Everyone has folded, the game is over.')
+	quit()
+elif len(hands) == 1:
+	print('Only one player remains, they have won.')
+	quit()
+
+community.append(deck.draw())
+
+print('\nRound 3')
+
+hands[:] = [hand for hand in hands if not think(hand, community) == ['Fold']]
+
+print(f'After round 3, {len(hands)} players remain')
+
+if len(hands) == 0:
+	print('Everyone has folded, the game is over.')
+	quit()
+elif len(hands) == 1:
+	print('Only one player remains, they have won.')
+	quit()
 
 
 
--- better_score.py
+++ better_score.py
@@ -6,10 +6,10 @@ def scoring(community, hands):
             formated_cards.append(str(ranks[card.value-2]+card.suit))
         return formated_cards
 
-    def evaluate(hand):
+    def evaluate(hand, community):
         ranks = '23456789TJQKA'
         rcounts = {ranks.find(r): ''.join(hand).count(r) for r, _ in hand}.items()
-        if len(hand) > 5: return max([evaluate(hand[:i] + hand[i+1:]) for i in range(len(hand))]) # a comparison here could determine if top 5 cards are from community, if so then return second highest (instead of max)
+        if len(hand) > 5: return max([evaluate(hand[:i] + hand[i+1:], community) for i in range(len(hand))]) 
         score, ranks = zip(*sorted((cnt, rank) for rank, cnt in {ranks.find(r): ''.join(hand).count(r) for r, _ in hand}.items())[::-1])
         if len(score) == 5:
             if ranks[0:2] == (12, 3): #adjust if 5 high straight
@@ -18,14 +18,15 @@ def scoring(community, hands):
             flush = len({suit for _, suit in hand}) == 1
             '''no pair, straight, flush, or straight flush'''
             score = ([(1,), (3,1,1,1)], [(3,1,1,2), (5,)])[flush][straight]
+        if set(hand) == set(community): # I think this should prevent someone from winning by using only community cards (Really hard to tell if it works tho)
+            score = (0,)
         return score, ranks
 
     scores = []
     community = formatting(community)
     for i, hand in enumerate(hands):
         hand = formatting(hand)
-        scores.append((i, evaluate(hand+community)))
+        scores.append((i, evaluate(hand+community, community)))
     winner = sorted(scores , key=lambda x:x[1])[-1][1]
     return [x[0] for x in filter(lambda x: x[1] == winner, scores)]
-
 
